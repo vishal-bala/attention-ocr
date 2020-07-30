2b036ab0bf3fe7f696096cfc8a986e2512181c86
Jakub Czerny <jakub.czerny@automationhero.ai>
2020-07-30 13:18:52 +0200

    Updated setup.py to reference requirements.txt

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import find_packages
 from setuptools import setup
 
-REQUIRED_PACKAGES = ['distance', 'numpy', 'six', 'pillow']
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 VERSION = '0.7.6'
 try:
     import pypandoc
     README = pypandoc.convert('README.md', 'rst')
 except(IOError, ImportError):
     README = open('README.md').read()
-
 
 setup(
     name='aocr',
@@ -17,7 +18,7 @@ setup(
     author='Ed Medvedev',
     author_email='edward.medvedev@gmail.com',
     version=VERSION,
-    install_requires=REQUIRED_PACKAGES,
+    install_requires=requirements,
     packages=find_packages(),
     include_package_data=True,
     license='MIT',
